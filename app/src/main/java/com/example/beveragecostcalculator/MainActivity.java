62e5a20d798b8fec13643396deace9e2b6f27b10
Saim Ahmad <Sahmad5349@conestogac.on.ca>
2019-10-13 13:28:14 -0400

    Project Complete

--- app/src/main/java/com/example/beveragecostcalculator/MainActivity.java
+++ app/src/main/java/com/example/beveragecostcalculator/MainActivity.java
@@ -22,17 +22,15 @@ public class MainActivity extends AppCompatActivity {
             "Quebec", "Nova Scotia", "New Brunswick", "Alberta", "Manitoba", "Newfoundland and Labrador", "Ontario", "Prince Edward Island", "Saskatchewan"
     };*/
 
+    //declaring the variables
     private RadioGroup  radioGroupBeverages;
     private RadioGroup radioGroupFlavor;
-    private RadioButton radioButtonTea;
-    private RadioButton radioButtonCoffee;
     private RadioButton radioButtonNone;
     private RadioButton radioButtonVanilla;
     private RadioButton radioButtonChocolate;
     private RadioButton radioButtonMint;
     private RadioButton radioButtonLemon;
-   // private RadioButton selectedBeverage;
-    private RadioButton selectedFlavor;
+    //private RadioButton selectedFlavor;
     private  EditText name;
     private CheckBox milk;
     private CheckBox sugar;
@@ -42,28 +40,29 @@ public class MainActivity extends AppCompatActivity {
     String cupSize = "";
     String flavor = "";
     String selectedBeverages = "";
+    String selectedFlavor = "";
     boolean needMilk = false;
     boolean needSugar =false;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Setting up the value for the Spinner using Array Adapter and using dropdown menu
         Spinner spin = findViewById(R.id.spinnerSize);
         ArrayAdapter<CharSequence> adapterSpinner = ArrayAdapter.createFromResource(this,R.array.size,android.R.layout.simple_spinner_item);
         adapterSpinner.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spin.setAdapter(adapterSpinner);
 
+
+        //setting up value for AutoComplete Text view using Array Adapter
         String[] provinces = getResources().getStringArray(R.array.provinces);
         AutoCompleteTextView act = findViewById(R.id.autoTextViewProvinces);
         ArrayAdapter<String> adapterAutoComplete = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, provinces);
         act.setAdapter(adapterAutoComplete);
 
         //mapping of all UI elements to variables
-        radioButtonCoffee =  findViewById(R.id.radioCoffee);
-        radioButtonTea = findViewById(R.id.radioTea);
         radioGroupBeverages = findViewById(R.id.radioGroupBeverage);
         radioGroupFlavor = findViewById(R.id.radioGroupFlavors);
         radioButtonNone =  findViewById(R.id.radioNone);
@@ -78,8 +77,6 @@ public class MainActivity extends AppCompatActivity {
         milk = findViewById(R.id.checkboxMilk);
         sugar = findViewById(R.id.checkboxSugar);
 
-
-
         //Disabling the radio buttons according to beverage type
         radioGroupBeverages.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
@@ -87,7 +84,6 @@ public class MainActivity extends AppCompatActivity {
             public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
                 switch (checkedId){
                     case R.id.radioCoffee:     //if coffee radio button was selected, disable mint n lemon option
-                        Toast.makeText(getApplicationContext(),"coffee is selected",Toast.LENGTH_SHORT).show();
                         radioButtonLemon.setEnabled(false);
                         radioButtonMint.setEnabled(false);
                         radioButtonNone.setEnabled(true);
@@ -97,7 +93,6 @@ public class MainActivity extends AppCompatActivity {
                         break;
 
                     case  R.id.radioTea:    //if tea radio button was selected, disable vanilla n chocolate option
-                        Toast.makeText(getApplicationContext(),"tea is selected",Toast.LENGTH_SHORT).show();
                         radioButtonChocolate.setEnabled(false);
                         radioButtonVanilla.setEnabled(false);
                         radioButtonNone.setEnabled(true);
@@ -113,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
         }); //end of listener for beverageRadio
 
 
-        //checking which cup size was selected
+        //checking which cup size is selected by user
         size.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -125,46 +120,68 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        //this will get the id of the radio button in flavor group which was selected
 
-        //this method will be called when calculate button is called
+        //setting value of selectedFlavor chosen by user
+        radioGroupFlavor.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioChocolate:
+                        selectedFlavor = "Chocolate";
+                        break;
+                    case  R.id.radioVanilla:
+                        selectedFlavor = "Vanilla";
+                        break;
+                    case  R.id.radioLemon:
+                        selectedFlavor = "Lemon";
+                        break;
+                    case  R.id.radioMint:
+                        selectedFlavor = "Mint";
+                        break;
+                    case  R.id.radioNone:
+                        selectedFlavor = "None";
+                        break;
+                }//end of switch
+            }
+        });
+
+        //this setOnClickListener method will be called when calculate button is called
         calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
+                //checking and setting up value if the checkbox for milk n sugar was checked
                 if(milk.isChecked())
                     needMilk =true;
                 if(sugar.isChecked())
                     needSugar=true;
 
-                int flavorId = radioGroupFlavor.getCheckedRadioButtonId();
-                selectedFlavor = ((RadioButton)findViewById(flavorId));
+                //calculation starts only iff all fields are filled
+                if(name.getText().toString().isEmpty() || selectedBeverages.isEmpty()|| selectedFlavor.isEmpty()|| cupSize.isEmpty()||province.getText().toString().isEmpty())
+                {
+                    Toast.makeText(getApplicationContext(),"Please Enter all details",Toast.LENGTH_LONG).show();
+                }
+                else{
+                    //calling this function will compute the price of beverage
+                    //this 'calculateBill' function has all the parameters required for computation
+                    calculateBill(name.getText().toString(),selectedBeverages,selectedFlavor,cupSize,needMilk,needSugar,province.getText().toString());
 
-                calculateBill(name.getText().toString(),selectedBeverages,selectedFlavor.getText().toString(),cupSize,needMilk,needSugar,province.getText().toString());
+                }
+
             }
         });
 
-
-
-
-
-
-
-
-
     }// end of onCreate
 
+    //calculateBill function is defined and total cost is calculated
     public void calculateBill(String name,String selectedBeverage ,String selectedFlavor,String cupSize,boolean needMilk,boolean needSugar, String province)
     {
-        if(name.equals("") || selectedBeverage.equals("")|| selectedFlavor.equals("")|| cupSize.equals("")||province.equals(""))
-        {
-            Toast.makeText(getApplicationContext(),"Please Enter all details",Toast.LENGTH_LONG).show();
-        }
 
-        else
-        {
             double cost = 0.0;
             String milky ="without milk";
             String sugary ="without sugar";
+            //adding the cost depending on the size of the cup
             if(size.equals("Small"))
             {
                 cost = 1.50;
@@ -176,7 +193,7 @@ public class MainActivity extends AppCompatActivity {
             else
                 cost= 3.25;
 
-            //processing and price calculation starts below depending on flavor of beverage
+            ////adding the cost depending on the flavor selected
             if(selectedFlavor.equals("Vanilla"))
                 cost += 0.25;
             else if (selectedFlavor.equals("Chocolate"))
@@ -186,12 +203,11 @@ public class MainActivity extends AppCompatActivity {
             else if (selectedFlavor.equals("Mint"))
                 cost+=0.50;
 
+            //adding the cost if user wants milk and sugar
             if(needMilk){
                 cost+= 1.25;
                 milky = "with milk";
             }
-
-
             if(needSugar){
                 cost+=1.00;
                 sugary = "with sugar";
@@ -200,12 +216,13 @@ public class MainActivity extends AppCompatActivity {
 
             //adding 13% tax to the cost of beverage
             double totalAmount = cost + (0.13*cost);
+            double finalAmount = Math.round(totalAmount*100.0)/100.0; //rounding it 2 decimal places
 
-             String output = "For "+name+"from "+province+", a "+cupSize+selectedBeverage+","+milky+"and "+sugary+", "+selectedFlavor+" cost: $"+totalAmount;
+             String output = "For "+name+" from "+province+", a "+cupSize+" "+selectedBeverage+","+milky+" and "+sugary+", "+selectedFlavor+" cost: $"+finalAmount;
 
             Toast.makeText(getApplicationContext(),output,Toast.LENGTH_LONG).show();
 
-        }
+
 
 
 
